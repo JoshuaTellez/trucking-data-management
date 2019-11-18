0ef06efc5625f5bee87cadd9a6cdd1132dc5cec4
joshtel <joshtel@live.com>
2019-11-17 22:18:54 -0800

    add models to tdm

--- tdmproject/tdm/admin.py
+++ tdmproject/tdm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Driver, Customer, Load
+
+admin.site.register(Driver)
+admin.site.register(Customer)
+admin.site.register(Load)
