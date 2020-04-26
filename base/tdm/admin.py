00db8ee6530bd79167c0e1bedb81dc5b04fb8d0b
joshtel <joshtel@live.com>
2020-04-26 15:42:22 -0700

    register models in admin site

--- base/tdm/admin.py
+++ base/tdm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Carrier, Driver, Customer, Load
 
 # Register your models here.
+admin.site.register(Carrier)
+admin.site.register(Driver)
+admin.site.register(Customer)
+admin.site.register(Load)
