ddf65ea5c35ebc69bf94607d92f477731206995f
joshtel <joshtel@live.com>
2020-04-26 19:00:15 -0700

    initial models migratiuon

--- base/tdm/models.py
+++ base/tdm/models.py
@@ -51,7 +51,12 @@ class Driver(models.Model):
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        full_name = ''
+        if self.first_name:
+            full_name += self.first_name + ' '
+        if self.last_name:
+            full_name += self.last_name
+        return full_name
 
 
 class Customer(models.Model):
